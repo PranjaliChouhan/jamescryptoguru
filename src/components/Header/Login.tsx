8955b01aa4b1dc17ea60c39e079b06b5ee1ad882
PranjaliChouhan <pranjalichouhan4@gmail.com>
2025-01-22 02:23:42 +0530

    update

--- src/components/Header/Login.tsx
+++ src/components/Header/Login.tsx
@@ -78,7 +78,7 @@ export function LoginForm({ onClose }: LoginFormProps) {
             {isPending ? 'Signing in...' : 'Sign in'}
           </Button>
           <div className="text-center text-sm text-gray-500">
-            Don't have an account?{' '}
+            Don&apos;t have an account?{' '}
             <Button 
               variant="link" 
               className="p-0 h-auto font-semibold text-primary"
